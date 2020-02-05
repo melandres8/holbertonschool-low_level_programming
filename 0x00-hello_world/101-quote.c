1c3cf69194335a9978749d8e5f841e8bf517d87e
melandres8 <melanmosta@gmail.com>
2020-02-05 23:18:44 +0000

    Adding the last advanced task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * main - Writing a quote
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	write(1, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
+}
