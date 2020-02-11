7784d4cb8f7e7b456af3595986fffeed766d5ef4
melandres8 <melanmosta@gmail.com>
2020-02-11 19:34:02 +0000

    First task of debbugins

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * main - Print if the number es 0
+ * Return: 0
+ * @i: Contains the value
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
