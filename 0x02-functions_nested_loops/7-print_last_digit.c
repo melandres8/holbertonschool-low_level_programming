5e94acec425ffefa49d6cd7bf3df5b06e5dcea11
melandres8 <melanmosta@gmail.com>
2020-02-11 06:47:20 +0000

    fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,18 +2,18 @@
 /**
  * print_last_digit - print the last digit of a number
  * Return: last digit
- *
  * @num: contain the value of the num
  **/
 int print_last_digit(int num)
 {
 	int last;
+	last = num % 10;
 
 	if (num <= 0)
 	{
-		num = num * -1;
+		last = last * -1;
 	}
-	last = num % 10;
+	
 	_putchar(last + '0');
 	return (last);
 }
