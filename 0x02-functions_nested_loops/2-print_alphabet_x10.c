992e183c5f3d55bb68f068a954a27cd48c95d42c
melandres8 <melanmosta@gmail.com>
2020-02-11 05:29:01 +0000

    Fix the task #2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,20 @@
 #include "holberton.h"
-
+/**
+ * print_alphabet_x10 - Print 10 the alphabet
+ **/
 void print_alphabet_x10(void)
 {
-	char alph;
+	char alph = 'a';
 	int i;
-	
+
 	for (i = 0; i < 10; i++)
 	{
-		for (alph = 'a'; alph <= 'z'; alph++)
+		while (alph <= 'z')
 		{
 			_putchar(alph);
+			alph++;
 		}
 		_putchar('\n');
+		alph = 'a';
 	}
 }
