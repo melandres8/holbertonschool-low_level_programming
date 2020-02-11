ba9193a5b9cfc22f4e2739297eb663c1f9e18ba6
melandres8 <melanmosta@gmail.com>
2020-02-11 05:18:45 +0000

    Fix task 0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,19 +1,18 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
  * main - Print Holberton
  * Return: Always 0 (Success)
  **/
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
+	int i;
+	char str[9] = "Holberton";
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(str[i]);
+	}
 	_putchar('\n');
 	return (0);
 }
