0aceb32613ec4a9398ff26edb220f63cbe05378c
melandres8 <melanmosta@gmail.com>
2020-02-13 18:43:00 +0000

    Task #7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_diagonal - Print a diagonal lines
+ *
+ **/
+void print_diagonal(int n)
+{
+	int n1, n2;
+
+	for (n1 = 0; n1 < n; n1++)
+	{
+		for (n2 = 0; n2 < n1; n2++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}
