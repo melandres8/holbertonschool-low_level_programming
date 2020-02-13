cacc61595afbb43b801cb4b0d367f6163573ecbb
melandres8 <melanmosta@gmail.com>
2020-02-13 20:51:20 +0000

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - I don't know
+ * Return: 0
+ **/
+int main(void)
+{
+	int counter;
+
+	for (counter = 1; counter <= 100; counter++)
+	{
+		if ((counter % 3 == 0) && (counter % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+
+		if (counter % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+
+		if (counter % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		if (!(counter % 3 == 0) && !(counter % 5 == 0))
+		{
+			printf("%d ", counter);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
