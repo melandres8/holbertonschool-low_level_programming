f03d365ce5c4088897baa849149ce9ef7d3de38a
melandres8 <melanmosta@gmail.com>
2020-02-14 04:01:19 +0000

    First advacned task

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *
+ *
+ **/
+int main(void)
+{
+	long i = 2;
+	long num = 612852475143;
+
+	while (i < num)
+	{
+		if (num % i == 0)
+		num /= i;
+		i++;
+	}
+	printf("%li\n", num);
+	return (0);
+}
