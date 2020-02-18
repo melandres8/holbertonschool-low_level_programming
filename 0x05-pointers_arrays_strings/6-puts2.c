c8f96505e7105a6bd91f2c5c67b459ef92f2dd37
melandres8 <melanmosta@gmail.com>
2020-02-18 16:52:06 +0000

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,11 +1,20 @@
 #include "holberton.h"
 
 /**
- * puts2 -
- *
- * Return: 
+ * puts2 - Puts only the multiplos of 2
+ * @str: Numbers from 0 to 10
+ * Return: Nothing
  */
 void puts2(char *str)
 {
+	int i;
 
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
 }
