c79db8ef7ef5d68b483806d1392fbb8780260811
melandres8 <melanmosta@gmail.com>
2020-02-18 02:13:48 +0000

    I got this

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,16 @@
 #include "holberton.h"
 
 /**
- * swap_int -
- *
- * Return: 
+ * swap_int - Swap of values
+ * @a: Pointer to an int
+ * @b: Pointer to an int
+ * Return: Nothing
  */
-void swap_int(int *a, int *b)
+void swap_int(int *a, int *b)	/*Swap *a and *b*/
 {
+	int var;
 
+	var = *a;
+	*a = *b;
+	*b = var;
 }
