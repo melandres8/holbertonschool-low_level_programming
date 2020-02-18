7a07cd31427b04f03a60497dc0944aba9259d5a1
melandres8 <melanmosta@gmail.com>
2020-02-18 02:26:34 +0000

    Strlen, return the length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,17 @@
 #include "holberton.h"
 
 /**
- * _strlen -
- *
- * Return: 
+ * _strlen - Calculate the value of a string
+ * @s: Pointer to a char
+ * Return: the value
  */
 int _strlen(char *s)
 {
+	int i;
 
+	for (i = 0; *s != '\0'; s++)
+	{
+		i++;
+	}
+	return (i);
 }
