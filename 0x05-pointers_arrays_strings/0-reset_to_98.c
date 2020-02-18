0ba7e0128ac23676c1bee3efbb0410c30aa56828
melandres8 <melanmosta@gmail.com>
2020-02-18 02:13:14 +0000

    First task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
 
 /**
- * reset_to_98 -
- *
- * Return: 
+ * reset_to_98 - Update the value
+ * @n: Puntero to an integer
+ * Return: Nothing
  */
 void reset_to_98(int *n)
 {
-
+	*n = 98;
 }
