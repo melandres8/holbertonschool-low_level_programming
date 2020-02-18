2c8aa551ee65f28aef7bb3843ddf9146b2e4b513
melandres8 <melanmosta@gmail.com>
2020-02-18 02:53:40 +0000

    Print a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,11 +1,17 @@
 #include "holberton.h"
 
 /**
- * _puts -
- *
- * Return: 
+ * _puts - Write a string with a new line at the end
+ * @str: Value of a string
+ * Return: nothing
  */
 void _puts(char *str)
 {
+	int i;
 
+	for (i = 0; str[i]; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
