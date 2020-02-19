b3367604115e531dfc9e1e4781cb0338d32c35c6
melandres8 <melanmosta@gmail.com>
2020-02-19 01:55:56 +0000

    First try

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,11 +1,20 @@
 #include "holberton.h"
 
 /**
- * _strcpy -
- *
- * Return: 
+ * _strcpy - Copie a string pointed
+ * @dest: Destination
+ * @: Sender
+ * Return: A char
  */
 char *_strcpy(char *dest, char *src)
 {
+	int counter;
 
+	for (counter = 0; src[counter] != '\0'; counter++)
+	{
+		dest[counter] = src[counter];
+	}
+	dest[counter] = '\0';
+
+	return (dest);
 }
