438ce446ce8c28789280853e3e7a7607a97a4775
melandres8 <melanmosta@gmail.com>
2020-02-19 02:04:22 +0000

    First tryyy

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,11 +1,21 @@
 #include "holberton.h"
 
 /**
- * puts_half -
- *
- * Return: 
+ * puts_half - Print the middle of the string
+ * @str: Contain the string
+ * Return: nothing
  */
 void puts_half(char *str)
 {
+	int i;
 
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+
+	for (i = i / 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
