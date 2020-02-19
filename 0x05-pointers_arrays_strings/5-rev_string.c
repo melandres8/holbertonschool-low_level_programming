ff82187814f2695483fe7acce5ed10623ecc5686
melandres8 <melanmosta@gmail.com>
2020-02-19 00:30:10 +0000

    First try

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,11 +1,20 @@
 #include "holberton.h"
-
 /**
- * rev_string -
  *
- * Return: 
- */
+ *
+ **/
 void rev_string(char *s)
 {
+	int i, c;
+	char rev;
 
+	for (i = 0; s[i] != '\0'; i++)
+	{}
+
+	for (c = 0, i--; c < i; c++, i--)
+	{
+		rev = s[c];
+		s[c] = s[i];
+		s[i] = rev;
+	}
 }
