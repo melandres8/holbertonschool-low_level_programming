2c4a7f69fc48f11104dab162b08bf2a7aafd341a
melandres8 <melanmosta@gmail.com>
2020-02-21 04:10:23 +0000

    Scare movie

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * cap_string - Capitalize an string
+ * @s: String
+ * Return: A char
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[0] >= 'a' && s[0] <= 'z')
+		{
+			s[0] = s[0] - 32;
+		}
+		else if ((s[i] == ' ') && s[i + 1] >= 'a' && s[i + 1] <= 'z')
+		{
+			s[i + 1] = s[i + 1] - 32;
+		}
+		else if ((s[i] == ';') && s[i + 1] >= 'a' && s[i + 1] <= 'z')
+		{
+			s[i + 1] = s[i + 1] - 32;
+		}
+		else if ((s[i] == '\n') && s[i + 1] >= 'a' && s[i + 1] <= 'z')
+		{
+			s[i + 1] = s[i + 1] - 32;
+		}
+		else if ((s[i] == '.') && s[i + 1] >= 'a' && s[i + 1] <= 'z')
+		{
+			s[i + 1] = s[i + 1] - 32;
+		}
+		i++;
+	}
+	return (s);
+}
