4f409c414e12f23e8be734d8889c191736926469
melandres8 <melanmosta@gmail.com>
2020-03-03 22:34:08 +0000

    First try

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,49 @@
+#include "holberton.h"
+#include <string.h>
+
+/**
+ * _strlen - Calculate the value of a string
+ * @s: Pointer to a char
+ * Return: the value
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; s++)
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _strdup - Duplicate a String.
+ * @str: String.
+ * Return: A char.
+ */
+char *_strdup(char *str)
+{
+	int size, i;
+	char *dest;
+
+	size = _strlen(str);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	dest = (char *) malloc(sizeof(str) * size + 1);
+
+	if (dest != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			dest[i] = str[i];
+		}
+	}
+	else
+	{
+		return (NULL);
+	}
+	return (dest);
+}
