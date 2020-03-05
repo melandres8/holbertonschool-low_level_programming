de52d471a6d10c92a7280b3d3f3767697ece8c0c
melandres8 <melanmosta@gmail.com>
2020-03-05 19:29:54 +0000

    First try

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,67 @@
+#include "holberton.h"
+
+/**
+ * _strlen - Calculate the value of a string
+ * @s: Pointer to a char
+ * Return: the value
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; s++)
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * string_nconcat - Concatenate two string.
+ * @s1:
+ * @s2:
+ * @n:
+ * Return: 
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int len1, len2;
+	unsigned int count = 0;
+	char *new;
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		s1 = s2 = "";
+	}
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+
+	new = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	if (n < len2)
+	{
+		len2 = n;
+	}
+	if (count < n)
+	{
+		while (*s1 != '\0')
+		{
+			new[count] = *s1;
+			count++;
+			s1++;
+		}
+		while (*s2 != '\0')
+		{
+			new[count] = *s2;
+			count++;
+			s2++;
+		}
+	}
+	new[len1 + len2 + 1] = '\0';
+	return (new);
+}
