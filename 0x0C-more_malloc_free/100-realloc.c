accae2911d4a8a46857c52cc0d9b21ed304fcf92
melandres8 <melanmosta@gmail.com>
2020-03-06 00:07:08 +0000

    asda

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,16 +9,16 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    unsigned int count;
+	unsigned int count;
 
-    count = 0;
-    while (count < n)
-    {
-        dest[count] = src[count];
-        count++;
-    }
+	count = 0;
+	while (count < n)
+	{
+		dest[count] = src[count];
+		count++;
+	}
 
-    return (dest);
+	return (dest);
 }
 
 /**
@@ -30,32 +30,28 @@ char *_memcpy(char *dest, char *src, unsigned int n)
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-    void *dest;
+	void *dest;
 
-    ptr = malloc(old_size);
-
-    if (new_size == old_size)
-    {
-        return (ptr);
-    }
-    if (ptr == NULL)
-    {
-        free(ptr);
-        dest = malloc(new_size);
-        return (dest);
-    }
-    if (new_size == 0 && ptr != NULL)
-    {
-        free(ptr);
-        return (NULL);
-    }
-    dest = malloc(new_size);
-    if (dest == NULL)
-    {
-        return (NULL);
-    }
-    free(ptr);
-    _memcpy(dest, ptr, old_size);
-    return (dest);
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	if (ptr == NULL)
+	{
+		dest = malloc(new_size);
+		return (dest);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	dest = malloc(new_size);
+	if (dest == NULL)
+	{
+		return (NULL);
+	}
+	free(ptr);
+	_memcpy(dest, ptr, old_size);
+	return (dest);
 }
-
