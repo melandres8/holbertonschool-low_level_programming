8e62b5073ad53abddf2305e563f264d59d312f7c
melandres8 <melanmosta@gmail.com>
2020-03-10 04:13:12 +0000

    adsads

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,17 +2,17 @@
 #define DOG
 
 /**
- * struct dog - Data of dog.
+ * struct dog_t - Data of dog.
  * @name: Name of dog.
  * @owner: Of the dog
  * @age: Age of the dog.
  */
-struct dog
+typedef struct dog_t
 {
 	char *name;
 	char *owner;
 	float age;
-};
+} dog_t;
 
 /*Prototypes*/
 void init_dog(struct dog *d, char *name, float age, char *owner);
