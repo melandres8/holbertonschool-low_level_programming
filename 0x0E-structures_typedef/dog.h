dd43489b48e099ae806c72390f55a230751027a6
melandres8 <melanmosta@gmail.com>
2020-03-10 03:28:51 +0000

    adadsd

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - Data of dog.
+ * @name: Name of dog.
+ * @owner: Of the dog
+ * @age: Age of the dog.
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
