65b19b0234b4d88692137f5008481566a03a5028
melandres8 <melanmosta@gmail.com>
2020-03-11 01:20:44 +0000

    asda Update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,4 +15,8 @@ typedef struct dog_t
 	char *owner;
 } dog_t;
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
