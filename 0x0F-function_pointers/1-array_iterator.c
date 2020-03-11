6900cfa1d0738bfde7e4ac76e9e1bf3ed0b22d4b
melandres8 <melanmosta@gmail.com>
2020-03-11 18:23:17 +0000

    fix

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,8 +11,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t count;
 
-	for (count = 0; count < size; count++)
+	if (array && action)
 	{
-		(*action)(array[count]);
+		for (count = 0; count < size; count++)
+		{
+			(*action)(array[count]);
+		}
 	}
 }
