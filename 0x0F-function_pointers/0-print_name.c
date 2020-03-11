8190a727c23b4b8f3ca368e961e74f9d23e127cb
melandres8 <melanmosta@gmail.com>
2020-03-11 16:51:07 +0000

    First task

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Print a name.
+ * @name: Name
+ * @f: Function.
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
