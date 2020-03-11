85941451798dfbc251f568d45a6ee89154e85e9a
melandres8 <melanmosta@gmail.com>
2020-03-11 01:23:35 +0000

    asdasd

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * free_dog - Frees dog.
+ * @d: Pointer to a struct
+ * Return: Nothing.
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
