c871c7e1f7fe9b09d4a780732cbd529c1dbe4788
melandres8 <melanmosta@gmail.com>
2020-03-11 01:39:10 +0000

    adasd

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,18 +3,21 @@
 #include <stdio.h>
 
 /**
- * struct dog_t - Data of dog.
+ * struct dog - Data of dog.
  * @name: Name of dog.
  * @owner: Of the dog
  * @age: Age of the dog.
  */
-typedef struct dog_t
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 } dog_t;
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
