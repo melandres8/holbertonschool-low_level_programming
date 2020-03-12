b665a7e7e704b4306cce3a16fffb5fffa4a911ab
melandres8 <melanmosta@gmail.com>
2020-03-12 04:11:12 +0000

    Adding and fixing

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,34 @@
+#include "3-calc.h"
+
+/**
+ * main - Entry point.
+ * @argc: Number of arguments
+ * @argv: Arguments
+ * Return: An int value.
+ */
+int main(int argc, char **argv)
+{
+	int (*func_ptr)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ((*argv[3] == '0') && (strcmp(argv[2], "/") == 0 ||
+		strcmp(argv[2], "%") == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	if (strcmp(argv[2], "*") && strcmp(argv[2], "/")
+		&& strcmp(argv[2], "-") && strcmp(argv[2], "+")
+		&& strcmp(argv[2], "%"))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	func_ptr = get_op_func(argv[2]);
+	printf("%d\n", func_ptr(atoi(argv[1]), atoi(argv[3])));
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Function that add two different values of int.
+ * @a: First integer value
+ * @b: Secund integer value
+ * Return: The add between two integers
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Function that subs two different values of int.
+ * @a: First integer value
+ * @b: Secund integer value
+ * Return: The subs between two integers
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Function that multiplicate two different values of int.
+ * @a: First integer value
+ * @b: Secund integer value
+ * Return: The mul between two integers
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Function that divide two different values of int.
+ * @a: First integer value
+ * @b: Secund integer value
+ * Return: The div between two integers
+ */
+int op_div(int a, int b)
+{
+	if ((a / b) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - Function that module two different values of int.
+ * @a: First integer value
+ * @b: Secund integer value
+ * Return: The mod between two integers
+ */
+int op_mod(int a, int b)
+{
+	if ((a % b) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
