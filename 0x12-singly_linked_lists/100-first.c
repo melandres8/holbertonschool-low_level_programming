6c9075b73a928040c3af04c5c93df3185709dee2
melandres8 <melanmosta@gmail.com>
2020-03-20 00:24:26 +0000

    Advanced task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * first - Printed first than main function
+ * Return: Nothing.
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
