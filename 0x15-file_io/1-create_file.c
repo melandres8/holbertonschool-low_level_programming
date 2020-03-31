e1d56c59e36f65fc5ed5c01551eaf2e530b9c367
melandres8 <melanmosta@gmail.com>
2020-03-31 21:07:49 +0000

    Removing a condition

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,11 +22,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-
-	if (o_file == -1)
-	{
-		return (-1);
-	}
+	
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len] != '\0'; len++)
