b39d17bb5ea7cfeff617deab89e8d39da58352a5
melandres8 <melanmosta@gmail.com>
2020-04-01 02:05:46 +0000

    ASDasdad

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,32 +19,32 @@ int main(int argc, char *argv[])
 
 	o_file1 = open(argv[1], O_RDONLY);
 	if (o_file1 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file%s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 
 	o_file2 = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
 	if (o_file2 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to file%s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 
 	while (r_file1 == 1024)
 	{
-		r_file1 = read(r_file1, buffer, 1024);
+		r_file1 = read(o_file1, buffer, 1024);
 		w_file2 = write(o_file2, buffer, r_file1);
 	}
 
 	if (r_file1 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file%s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	if (w_file2 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to file%s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 
 	if (close(o_file1) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd%d\n", o_file1);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", o_file1);
 		exit(100);
 	if (close(o_file2) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd%d\n", o_file2);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", o_file2);
 		exit(100);
 
 	return (0);
