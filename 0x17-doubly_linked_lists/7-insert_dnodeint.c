86725767c480b9a74cbcaf32af4f9d897c0bfea0
melandres8 <melanmosta@gmail.com>
2020-05-03 02:20:08 +0000

    betty ily

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -34,7 +34,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = new_node;
 		return (new_node);
 	}
-	for(i = 0; i < idx - 1; i++)
+	for (i = 0; i < idx - 1; i++)
 	{
 		if (tmp == NULL)
 		{
