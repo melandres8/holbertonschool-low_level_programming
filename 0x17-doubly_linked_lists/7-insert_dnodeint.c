37764f190ddc4520dd843f972ddbe2c3940d64b1
melandres8 <melanmosta@gmail.com>
2020-05-03 02:18:32 +0000

    Betty style and fix some issues

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -34,7 +34,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = new_node;
 		return (new_node);
 	}
-	while (count < idx - 1)
+	for(i = 0; i < idx - 1; i++)
 	{
 		if (tmp == NULL)
 		{
