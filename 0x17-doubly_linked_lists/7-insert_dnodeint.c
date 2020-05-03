3e6b3c78f11d0c2263d8896d940510016bdec2dd
melandres8 <melanmosta@gmail.com>
2020-05-03 02:15:51 +0000

    task #7

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,52 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - Inset a node in a given position
+ * @h: Head node
+ * @idx: Index
+ * @n: Number
+ * Return: It depends
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node, *tmp = *h;
+	unsigned int i = 0;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	if (tmp == NULL && idx > 0)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	else if (idx == 0)
+	{
+		if (tmp == NULL)
+			new_node->next = NULL;
+		else
+		{
+			new_node->next = tmp;
+			tmp->prev = new_node;
+		}
+		new_node->next = NULL;
+		*h = new_node;
+		return (new_node);
+	}
+	while (count < idx - 1)
+	{
+		if (tmp == NULL)
+		{
+			free(new_node);
+			return (NULL);
+		}
+		tmp = tmp->next;
+	}
+	new_node->next = tmp->next;
+	new_node->prev = tmp;
+	if (tmp->next)
+		tmp->next->prev = new_node;
+	tmp->next = new_node;
+	return (new_node);
+}
