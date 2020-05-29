4c2d282311a135d81de5ce602712d0394a5bffda
melandres8 <melanmosta@gmail.com>
2020-05-29 17:13:23 +0000

    {set} hash tables

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,40 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table.
+ * @ht: hash table
+ * @key: key
+ * @value: Value
+ * Return: 1 if it succeeded, 0 otherwise.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	int i;
+	hash_node_t *current, *new;
+
+	if (!ht || !key || !value)
+		return (0);
+	i = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[i];
+
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
+		return (0);
+
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = strdup(value);
+			return (1);
+		}
+		current = current->next;
+	}
+
+	new->key = strdup(key);
+	new->value = strdup(value);
+	new->next = ht->array[i];
+	ht->array[i] = new;
+	return (1);
+}
