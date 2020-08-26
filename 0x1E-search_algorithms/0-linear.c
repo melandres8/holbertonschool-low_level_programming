0ec55949f3f7af413fe23c3d8b6e1781dbd6c4ec
Melkin Mosquera <56529467+melandres8@users.noreply.github.com>
2020-08-26 13:00:41 -0500

    task#0: casting variable

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,9 +14,9 @@ int linear_search(int *array, size_t size, int value)
 	if (array == NULL)
 		return (-1);
 
-	for (count = 0; count < (int)size; count++)
+	for (count = 0; count < (signed int)size; count++)
 	{
-		printf("Value checked array[%d] = [%d]\n", count, array[count]);
+		printf("Value checked array[%i] = [%i]\n", count, array[count]);
 		if (array[count] == value)
 			return (count);
 	}
